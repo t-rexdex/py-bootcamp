99f79ed0721d63f535dd619cf7dbaa1bc7133efb
t-rexdex <tyler.dex@gmail.com>
2023-01-21 07:08:24 -0600

    formatted and added value check

--- school-catalogue/main.py
+++ school-catalogue/main.py
@@ -1,75 +1,85 @@
-class School():
-  initialStudents = 1
-  def __init__(self, name, level, numberOfStudents):
-    self.name = name
-    self.level = level
-    self.numberOfStudents = numberOfStudents
+class School:
+    initialStudents = 1
+  
+    
 
-  def get_name(self):
-    return self.name
+    def __init__(self, name, level, numberOfStudents):
+        self.name = name
+        if level not in ["primary", "middle", "high"]:
+            raise ValueError(f"Got {level}: expected primary, middle, or high")
+        self.level = level
+        self.numberOfStudents = numberOfStudents
 
-  def set_name(self, name):
-    self.name = name
+    def get_name(self):
+        return self.name
 
-  def get_level(self):
-    return self.level
+    def set_name(self, name):
+        self.name = name
 
-  def set_level(self, level):
-    self.level = level
+    def get_level(self):
+        return self.level
 
-  def get_NumOfStudents(self):
-    return self.numberOfStudents
+    def set_level(self, level):
+        self.level = level
 
-  def set_NumOfStudents(self, numberOfStudents):
-    self.numberOfStudents = numberOfStudents
+    def get_NumOfStudents(self):
+        return self.numberOfStudents
 
-  def __repr__(self):
-    return 'A {} school named {} with {} students.'.format(
-      self.level, 
-      self.name, 
-      self.numberOfStudents
-                  )
+    def set_NumOfStudents(self, numberOfStudents):
+        self.numberOfStudents = numberOfStudents
+
+    def __repr__(self):
+        return "A {} school named {} with {} students.".format(
+            self.level, self.name, self.numberOfStudents
+        )
+
+
 class Primary(School):
-  def __init__(self, name, numberOfStudents,pickupPolicy):
-    super().__init__(name = name, level="primary", numberOfStudents = numberOfStudents)
-    self.pickupPolicy = pickupPolicy
+    def __init__(self, name, numberOfStudents, pickupPolicy):
+        super().__init__(name=name, level="primary", numberOfStudents=numberOfStudents)
+        self.pickupPolicy = pickupPolicy
 
-  def __repr__(self):
-    parentRepr = super().__repr__()
-    return parentRepr + " The pickup policy is {pickupPolicy}".format(pickupPolicy = self.pickupPolicy)
+    def __repr__(self):
+        parentRepr = super().__repr__()
+        return parentRepr + " The pickup policy is {pickupPolicy}".format(
+            pickupPolicy=self.pickupPolicy
+        )
 
-  def getPickupPolicy(self):
-    return self.pickupPolicy
+    def getPickupPolicy(self):
+        return self.pickupPolicy
 
 
 class Middle(School):
-  def __init__(self, name, numberOfStudents):
-    super().__init__(name = name, level="middle", numberOfStudents = numberOfStudents)
+    def __init__(self, name, numberOfStudents):
+        super().__init__(name=name, level="middle", numberOfStudents=numberOfStudents)
 
 
 class High(School):
-  def __init__(self, name, numberOfStudents,sportsTeams):
-    super().__init__(name = name, level="high", numberOfStudents = numberOfStudents)
-    self.sportsTeams = sportsTeams
+    def __init__(self, name, numberOfStudents, sportsTeams):
+        super().__init__(name=name, level="high", numberOfStudents=numberOfStudents)
+        self.sportsTeams = sportsTeams
 
-  def __repr__(self):
-    parentRepr = super().__repr__()
-    return parentRepr + " The school's sports teams are {sportsTeams}".format(sportsTeams = self.sportsTeams)
+    def __repr__(self):
+        parentRepr = super().__repr__()
+        return parentRepr + " The school's sports teams are {sportsTeams}".format(
+            sportsTeams=self.sportsTeams
+        )
 
-  def getSportsTeams(self):
-    return self.sportsTeams
+    def getSportsTeams(self):
+        return self.sportsTeams
 
-a = Middle("Codecademy", 100)
+
+a = School("Codecademy", "high", 100)
 print(a)
 # print(a.get_name())
 # print(a.get_level())
 # a.set_NumOfStudents(200)
 # print(a.get_NumOfStudents())
 
-b = Primary("Codecademy", 300, "Pickup Allowed")
-# print(b.getPickupPolicy())
-print(repr(b))
+# b = Primary("Codecademy", 300, "Pickup Allowed")
+# # print(b.getPickupPolicy())
+# print(repr(b))
 
-c = High("Codecademy High", 500, ["Tennis", "Basketball"])
-print(c.getSportsTeams())
-print(c)
+# c = High("Codecademy High", 500, ["Tennis", "Basketball"])
+# print(c.getSportsTeams())
+# print(c)
