3f15c23de2647e2d14ee7f38167a33dde06c3236
t-rexdex <tyler.dex@gmail.com>
2023-02-17 16:52:43 -0600

    fill table function is working temp

--- nile_project/table_population/SQL_Helper.py
+++ nile_project/table_population/SQL_Helper.py
@@ -5,6 +5,7 @@ def create_connection(path_for_file: str) -> object :
     # create a way to test if file exists in directory
     try:
         if os.path.isfile(path_for_file):
+            os.remove(path_for_file)
             print('File already exists. Reestablishing connection')
         else:
             print('Creating database and establishing a connection')
@@ -19,7 +20,17 @@ def create_connection(path_for_file: str) -> object :
 def create_table(con, c, fields_dict: dict):
     '''
     Need to create a test file for testing these methods
+     tables = {
+        'Trip_info': {
+            'Trip_id': 'int PRIMARY KEY',
+            'name': 'str',
+            'source_location': 'str', 
+            'destination_location': 'str', 
+            'duration_mins': 'float', 
+        } # data to create tables
+    }
     '''
+
 
     try:
         for table in fields_dict:
@@ -43,3 +54,61 @@ def print_table_fields(con, c, fields_dict): # need to rework may need to also a
         data=c.execute("SELECT * FROM " + table ) # need to fix this line so sqlite can properly call the correct table
         for column in data.description:
             print(column[0])
+
+
+def check_size_of_insert_table_and_trip_log(table, trip_log):
+    '''
+    possibly create this method to check if values will successfully enter into db based on called table and trip_log
+    '''
+    pass
+
+
+def dict_to_list_for_import(some_dict: dict) -> list:
+    lst = []
+    for tple in some_dict.items():
+        outerlst = []   
+        for item in tple:
+            if type(item) == tuple:
+                for i in item:
+                    outerlst.append(i)
+            else:
+                outerlst.append(item)
+        lst.append(outerlst)
+    return lst
+
+def fill_table(con, c, some_dict, table_name = ""):
+    '''
+    Create a method to take in a dictionary and insert that into a table 
+    may need to reinsert the table that it used to create fields to fill in the (insert into table_name VALUES() columns)
+    having issues with the columns string. 
+    outputing as: (1, Alon, Street_5, Street_3, 200.0)
+    needs to be: (1, 'Alon', 'Street_5', 'Street_3', 200.0)
+    what do i need to do the have those parts of the string to be read correctly
+
+    # you can iterate through lst[i]
+
+
+            fields = "(" + ",   ".join(["{}".format(k) for k in list(fields_dict[table_name].keys())]) + ")"
+            columns = "(" + "".join(f"{outerlst[0]}, {outerlst[1]}, {outerlst[2]}, {outerlst[3]}, {outerlst[4]}") + ")"
+            c = con.cursor()
+            c.execute("INSERT INTO " + table_name + fields + "\nVALUES" + columns +";") 
+            # outputing as: (1, Alon, Street_5, Street_3, 200.0)
+            # needs to be: (1, 'Alon', 'Street_5', 'Street_3', 200.0) 
+            # hints it can be related to using fields_dict i believe if i do this itll allow me to use executemany
+            # maybe haing something that looks into fields dict at that index and if type is text then the in the trip log
+            # we should have the names, start and end should have an output to match 'Driver' 'Street_start' 'Street_end'
+            # that and we may also need to change the way the outerlist is read?
+                # print(tables['Trip_info'].values())
+                # print(tables['Trip_info'].keys())
+                # print(list(tables['Trip_info'].keys())[3:])
+
+
+        con.commit()
+
+
+# INSERT INTO table (column1,column2 ,..)
+# VALUES( value1,	value2 ,...);
+'''
+    # lst = dict_to_list_for_import(some_dict)
+    c.executemany("INSERT INTO " + table_name + " VALUES (?, ?, ?, ?, ?)", dict_to_list_for_import(some_dict))
+    con.commit()
--- nile_project/table_population/main.py
+++ nile_project/table_population/main.py
@@ -1,43 +1,67 @@
-# something something imports
+import sys
+from pprint import pprint  as pp
+import itertools
+
+sys.path.append('./nile_project')
+from table_population.table_main  import *
+import SQL_Helper
+
 
 def main():
-    # con, c = create_connection('./nile_project/table_population/file.db') # establish connection
+    number_of_total_streets = 50000
+    total_number_of_trips = 100000
+    min_distance = 50
+    max_distance = 800
+    drivers = ['Alon', 'Sarah', 'Hailey', 'Tyler', 'Chaos', 'Bertie', 'Shiner', 'Huy', 'Jon', 'Luis']
 
-    # tables = {
-    #     'Trip_info': {
-    #         'Trip_id': 'str PRIMARY KEY',
-    #         'name': 'str',
-    #         'source_location': 'str', 
-    #         'destination_location': 'str', 
-    #         'duration_mins': 'float', 
-    #     } # data to create tables
-    # }
+    street_mapping = create_street_mapping(number_of_total_streets, min_distance, max_distance)
+    tg1 = table_generation(min_distance, max_distance, total_number_of_trips, drivers)
+    trip_log = tg1.create_trip_log(street_mapping)
+    table_name = 'Trip_info'
+    # lst = SQL_Helper.dict_to_list_for_import(trip_log)
+    # print(lst)
+    # outputs -> [[1, 'Alon', 'Street_5', 'Street_3', 200.0], [2, 'Huy', 'Street_1', 'Street_2', 50.0], 
+    # [3, 'Jon', 'Street_3', 'Street_4', 200.0], [4, 'Chaos', 'Street_1', 'Street_3', 30.0], [5, 'Chaos', 'Street_2', 'Street_1', 50.0]]
+    tables = {
+        'Trip_info': {
+            'Trip_id': 'INTEGER PRIMARY KEY',
+            'name': 'TEXT',
+            'source_location': 'TEXT', 
+            'destination_location': 'TEXT', 
+            'duration_mins': 'REAL', 
+        } # data to create tables
+    }
+
+    # lst1 = SQL_Helper.dict_to_list_for_import(tables['Trip_info'])
+    print()
+    # outputs -> [['Trip_id', 'INTEGER PRIMARY KEY'], ['name', 'TEXT'], ['source_location', 'TEXT'], ['destination_location', 'TEXT'], ['duration_mins', 'REAL']]
+    # for column, obj_type in lst1:
+    # for array in lst:
+    #     for value in array:
+    #         print(value)
+    #         print(type(value))   
+    #         print('\n')
+    # print('\n')
+  # how to use this for loop to grab items from lst to insert properly in execute statement
 
 
-    # create_table(con, c, tables) 
-
-    # print_table_fields(con, c, tables) 
 
 
-    # ##### Playing with the code 
-    # print('Creating Street mapping dictionary')
-    # street_mapping = create_street_mapping_dictionary(5000) # mapping created need to insert this into db
-    # street_mapping = create_street_alt(500, 60, 800)
-    # print('Successfully created mapping dictionary')
+    con, c = SQL_Helper.create_connection('./nile_project/table_population/file.db') # establish connection
+    SQL_Helper.create_table(con, c, tables) 
+    # SQL_Helper.print_table_fields(con, c, tables) 
+    SQL_Helper.fill_table(con, c, trip_log, table_name = 'Trip_info')
 
-    # print('\n\n\n\n')
-    print('Creating Trip log dictionary')
-
-    trip_log = table_generation.create_trip_log(100000, ['Alon', 'Sarah', 'Hailey', 'Tyler', 'Chaos', 'Bertie', 'Shiner', 'Huy', 'Jon', 'Luis'], table_generation.create_street_mapping(500, 60, 800))
-
-    # print('This is true log')
-
-    print('Log is completed')
-    # pprint.pprint(trip_log)
+    for row in c.execute("select * from Trip_info"):
+        print(row)
 
 
+
+
+ 
 
 
 
 if __name__ == "__main__":
     main()   
+
