1c324f2a8fce17943ee0cc27c773b2a29f7ecf1d
t-rexdex <tyler.dex@gmail.com>
2023-02-24 09:42:46 -0600

    Updated with comments

--- nile_project/table_population/main.py
+++ nile_project/table_population/main.py
@@ -5,37 +5,40 @@ from SQL_Helper import mysqlite3 as ms3
 def main():
     # definitely need to condense these args 
     parser = argparse.ArgumentParser()
+    # should also add help contexts prompts? or is this overkill? I feel like it necessary to implement now for career's sake
     parser.add_argument('--streets')
     parser.add_argument('--trips')
     parser.add_argument('--min_distance')
     parser.add_argument('--max_distance')
     args = vars(parser.parse_args()) # if im using this and it becomes a dict perhaps I should refactor once again to take advantage of the new format
+# need to do something a little more intelligent with the type checking? 
+# should create a separate function to do that or should i put it within
 
-    number_of_total_streets = args['streets']
-    total_number_of_trips = args['trips']
-    min_distance = args['min_distance']
-    max_distance = args['max_distance']
+    number_of_total_streets = int(args['streets'])
+    total_number_of_trips = int(args['trips'])
+    min_distance = int(args['min_distance'])
+    max_distance = int(args['max_distance'])
     drivers = ['Alon', 'Sarah', 'Hailey', 'Tyler', 'Chaos', 'Bertie', 'Shiner', 'Huy', 'Jon', 'Luis']
-    print(type(args))
-    # street_mapping = create_street_mapping(number_of_total_streets, min_distance, max_distance)
 
-    # tg1 = table_generation(min_distance, max_distance, total_number_of_trips, drivers)
+    street_mapping = create_street_mapping(number_of_total_streets, min_distance, max_distance)
 
-    # trip_log = tg1.create_trip_log(street_mapping)
+    tg1 = table_generation(min_distance, max_distance, total_number_of_trips, drivers)
 
-    # table = {
-    #     'Trip_info': {
-    #         'Trip_id': 'INTEGER PRIMARY KEY',
-    #         'driver': 'TEXT',
-    #         'start_location': 'TEXT', 
-    #         'destination_location': 'TEXT', 
-    #         'duration_in_mins': 'REAL', 
-    #     }
-    # }
+    trip_log = tg1.create_trip_log(street_mapping)
 
-    # with ms3() as db:
-    #     db.create_table(table)
-    #     db.executemany(trip_log)
+    table = {
+        'Trip_info': {
+            'Trip_id': 'INTEGER PRIMARY KEY',
+            'driver': 'TEXT',
+            'start_location': 'TEXT', 
+            'destination_location': 'TEXT', 
+            'duration_in_mins': 'REAL', 
+        }
+    }
+
+    with ms3() as db:
+        db.create_table(table)
+        db.executemany(trip_log)
 
 if __name__ == "__main__":
     main()   
